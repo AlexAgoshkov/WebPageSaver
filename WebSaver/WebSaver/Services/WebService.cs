4f50646a9301d8bde5c67f96373a7688d5d4b26b
Alexey <alex.agoshkov6@gmail.com>
2020-03-07 21:44:02 +0200

    Get Request Changed
    Get Request Changed

--- WebSaver/WebSaver/Services/WebService.cs
+++ WebSaver/WebSaver/Services/WebService.cs
@@ -15,13 +15,45 @@ namespace WebSaver.Services
         {
             string result = "";
 
-            Uri uri = new Uri($"http://{addres}");
-            
-            using (WebClient client = new WebClient())
+            var message = $"GET / HTTP/1.0\nHost: {addres}\r\n\r\n";
+            try
             {
-                result = await client.DownloadStringTaskAsync($"http://{addres}");
-            }
+                var port = 80;//19539; //80
+                var serverAddr = addres; //"selin.in.ua"; //"0.tcp.ngrok.io";
+                var client = new TcpClient(serverAddr, port);
 
+                var data = Encoding.ASCII.GetBytes(message);
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(data, 0, data.Length);
+                stream.Flush();
+
+                var responseData = new byte[1024];
+                int bytesRead = await stream.ReadAsync(responseData, 0, responseData.Length);
+                var responseMessage = Encoding.ASCII.GetString(responseData, 0, bytesRead);
+
+                string[] words = responseMessage.Split(new char[] { '\n' });
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (words[i].StartsWith("<!Doctype") || words[i].StartsWith("<!DOCTYPE"))
+                    {
+                        for (int j = i; j < words.Length; j++)
+                        {
+                            result += words[j];
+                        }
+                        break;
+                    }
+                }
+
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Exp {ex}");
+            }
+            
             return result;
         }
     }
